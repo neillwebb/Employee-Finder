2849a8b0e8ad63580ebfcf33cb48c1452a314904
Neill Webb <neill.webb@gmail.com>
2019-03-28 09:35:12 -0400

    final commit

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
+
 const express = require('express');
 const path = require('path');
 
-// Sets up the Express App
-// =============================================================
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
 
-// parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-// parse application/json
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, './app/public')));
 
-// Routes
-// =============================================================
+
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
