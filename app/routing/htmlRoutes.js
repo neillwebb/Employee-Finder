dc16d5c419f0bd13be436411a06df57c7e4bc6c0
Neill Webb <neill.webb@gmail.com>
2019-03-28 09:54:42 -0400

    fixes path in server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,11 +4,11 @@ const path = require("path");
 module.exports = function(app) {
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey"))
+    res.sendFile(path.join(__dirname, "../public/home"))
 })
 
 app.get("/employees", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home"))
+    res.sendFile(path.join(__dirname, "../public/survey"))
 })
 
 } ;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, './app/public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 
 require('./app/routing/apiRoutes.js')(app);
